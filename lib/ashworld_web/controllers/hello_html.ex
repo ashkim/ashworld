c5796a904fc6059152df18f2aaefaa6e02e984f7
ashk <ash.kim@gmail.com>
2024-02-17 21:20:47 +1100

    added a User schema and some validation

--- lib/ashworld_web/controllers/hello_html.ex
+++ lib/ashworld_web/controllers/hello_html.ex
@@ -2,4 +2,12 @@ defmodule AshworldWeb.HelloHTML do
   use AshworldWeb, :html
 
   embed_templates "hello_html/*"
+
+  attr :messenger, :string, required: true
+
+  def greet(assigns) do
+    ~H"""
+    <h2>Hello World, from <%= @messenger %>! ❤️</h2>
+    """
+  end
 end
